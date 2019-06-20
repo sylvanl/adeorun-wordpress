51bd4cb57a8dbc19c2cb62319d6eac940ada2753
Sylvan Lavallee <geckoweb.webmaster@gmail.com>
2019-06-20 09:58:19 +0200

    options update

--- wp-content/themes/adeorun/functions.php
+++ wp-content/themes/adeorun/functions.php
@@ -453,21 +453,21 @@ function html5_shortcode_demo_2($atts, $content = null) // Demo Heading H2 short
 if( function_exists('acf_add_options_page') ) {
 	
 	acf_add_options_page(array(
-		'page_title' 	=> 'Theme General Settings',
-		'menu_title'	=> 'Theme Settings',
+		'page_title' 	=> 'Paramètres du thème',
+		'menu_title'	=> 'Paramètres du thème',
 		'menu_slug' 	=> 'theme-general-settings',
 		'capability'	=> 'edit_posts',
 		'redirect'		=> false
 	));
 	
 	acf_add_options_sub_page(array(
-		'page_title' 	=> 'Theme Header Settings',
+		'page_title' 	=> 'Paramètres du header',
 		'menu_title'	=> 'Header',
 		'parent_slug'	=> 'theme-general-settings',
 	));
 	
 	acf_add_options_sub_page(array(
-		'page_title' 	=> 'Theme Footer Settings',
+		'page_title' 	=> 'Paramètres du footer',
 		'menu_title'	=> 'Footer',
 		'parent_slug'	=> 'theme-general-settings',
 	));
