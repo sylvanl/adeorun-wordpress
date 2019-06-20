d7e5c0e39b5f62b4b3349d1e29d05530c60574e4
Sylvan Lavallee <geckoweb.webmaster@gmail.com>
2019-06-20 09:59:32 +0200

    options update

--- wp-content/themes/adeorun/functions.php
+++ wp-content/themes/adeorun/functions.php
@@ -454,7 +454,7 @@ if( function_exists('acf_add_options_page') ) {
 	
 	acf_add_options_page(array(
 		'page_title' 	=> 'Paramètres du thème',
-		'menu_title'	=> 'Paramètres du thème',
+		'menu_title'	=> 'Paramètres',
 		'menu_slug' 	=> 'theme-general-settings',
 		'capability'	=> 'edit_posts',
 		'redirect'		=> false
