1216e1a6cfa25b83f4a47871c102185583374378
Alexandre Lafouasse <a_lafouasse@etu-webschoolfactory.gt>
2019-06-21 14:38:51 +0200

    Essai page blog et CPT Documentation

--- wp-content/themes/adeorun/functions.php
+++ wp-content/themes/adeorun/functions.php
@@ -400,18 +400,18 @@ function create_post_type_html5()
     register_post_type('html5-blank', // Register Custom Post Type
         array(
         'labels' => array(
-            'name' => __('HTML5 Blank Custom Post', 'html5blank'), // Rename these to suit
-            'singular_name' => __('HTML5 Blank Custom Post', 'html5blank'),
-            'add_new' => __('Add New', 'html5blank'),
-            'add_new_item' => __('Add New HTML5 Blank Custom Post', 'html5blank'),
-            'edit' => __('Edit', 'html5blank'),
-            'edit_item' => __('Edit HTML5 Blank Custom Post', 'html5blank'),
-            'new_item' => __('New HTML5 Blank Custom Post', 'html5blank'),
-            'view' => __('View HTML5 Blank Custom Post', 'html5blank'),
-            'view_item' => __('View HTML5 Blank Custom Post', 'html5blank'),
-            'search_items' => __('Search HTML5 Blank Custom Post', 'html5blank'),
-            'not_found' => __('No HTML5 Blank Custom Posts found', 'html5blank'),
-            'not_found_in_trash' => __('No HTML5 Blank Custom Posts found in Trash', 'html5blank')
+            'name' => __('Documentation', 'html5blank'), // Rename these to suit
+            'singular_name' => __('Documentation', 'html5blank'),
+            'add_new' => __('Ajouter', 'html5blank'),
+            'add_new_item' => __('Ajouter une nouvelle documentation', 'html5blank'),
+            'edit' => __('Editer', 'html5blank'),
+            'edit_item' => __('Editer la documentation', 'html5blank'),
+            'new_item' => __('Nouvelle documentation', 'html5blank'),
+            'view' => __('Voir la documentation', 'html5blank'),
+            'view_item' => __('Voir la documentation', 'html5blank'),
+            'search_items' => __('Chercher la documentation', 'html5blank'),
+            'not_found' => __("Il n'y a pas de documentation", 'html5blank'),
+            'not_found_in_trash' => __('Pas de documentation trouvée dans la corbbeille', 'html5blank')
         ),
         'public' => true,
         'hierarchical' => true, // Allows your posts to behave like Hierarchy Pages
