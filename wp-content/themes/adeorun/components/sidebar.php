daf06edd5352b1ef025f871623f3f2d0d89ed885
Alexandre Lafouasse <a_lafouasse@etu-webschoolfactory.gt>
2019-06-21 10:47:42 +0200

    Update sidebar.php

--- wp-content/themes/adeorun/components/sidebar.php
+++ wp-content/themes/adeorun/components/sidebar.php
@@ -5,11 +5,16 @@
 	<a class="waves-effect waves-light btn" href='<?php the_field('button-url', 'options') ?>'><?php the_field('button-text', 'options') ?></a>
 
 	<!-- Facebook share button -->
-	<div id="fb-root"></div>
-	<script async defer crossorigin="anonymous" src="https://connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v3.3&appId=240618955988088&autoLogAppEvents=1"></script>
-	<div class="fb-share-button" data-href="<?php get_the_permalink(); ?>" data-layout="button" data-size="small"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Partager</a></div>
+	<div class="share_buttons">
 
-	
+		<div id="fb-root"></div>
+		<script async defer crossorigin="anonymous" src="https://connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v3.3&appId=240618955988088&autoLogAppEvents=1"></script>
+		<div class="fb-share-button" data-href="<?php get_the_permalink(); ?>" data-layout="button" data-size="small"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Partager</a></div>
+
+	<!-- Twitter share button -->
+		<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+	</div>
 
 </aside>
 <!-- /sidebar -->
