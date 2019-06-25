e5875c18adbe8afa4d2f069ad972ca84391d3217
Sylvan Lavallee <geckoweb.webmaster@gmail.com>
2019-06-25 22:04:33 +0200

    Blank page revamped

--- wp-content/themes/adeorun/blank.php
+++ wp-content/themes/adeorun/blank.php
@@ -1,15 +1,48 @@
-<?php  /* Template Name: BlankSS */ ?>
+<?php  /* Template Name: Blank */
+get_header(); ?>
 
-<?php get_template_part('components/head'); ?>
+<!-- Hide header and footer -->
+<style>
+  nav {
+    display:none;
+  } 
+  footer {
+    display:none;
+  }
+</style>
+<!-- /Hide header and footer -->
 
-  <main role="main">
-    <div class="container">
+<main role="main">
+		
+  <div class="container">
 
-      <section>
+    <h1><?php the_title(); ?></h1>
+
+    <?php if (have_posts()): while (have_posts()) : the_post(); ?>
+
+      <!-- article -->
+      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
         <?php the_content(); ?>
-      </section>
 
-    </div>
-  </main>
+      </article>
+      <!-- /article -->
 
-<?php get_template_part('components/scripts'); ?>
+    <?php endwhile; ?>
+
+    <?php else: ?>
+
+      <!-- article -->
+      <article>
+
+        <h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
+
+      </article>
+      <!-- /article -->
+
+    <?php endif; ?>
+
+  </div>
+</main>
+
+<?php get_footer(); ?>
