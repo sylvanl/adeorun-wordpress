9905389eab25d5b2662057451a1df15b3f018918
Alexandre Lafouasse <a_lafouasse@etu-webschoolfactory.gt>
2019-06-25 10:26:05 +0200

    Indentation & sections

--- wp-content/themes/adeorun/single-documentation.php
+++ wp-content/themes/adeorun/single-documentation.php
@@ -1,82 +1,65 @@
-
 <?php get_header(); ?>
 
 	<main role="main">
     <div class="container">
 
-	<!-- section -->
-<section>
-
-	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-
-		<!-- article -->
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
 
 			<!-- post title -->
-			<h1>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-			</h1>
-      <p><?php the_excerpt(); ?></p>
+			<section>
+
+				<h1>
+					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+				</h1>
+
+				<p><?php the_excerpt(); ?></p>
+
+			</section>
 			<!-- /post title -->
 
       <!-- Steps of the documentation -->
-      <?php if( have_rows('etapes') ): ?>
-      <?php $step = 1; ?>
-      	<ul>
-      	<?php while( have_rows('etapes') ): the_row();
+			<section>
 
-      		// vars
+	      <?php if( have_rows('etapes') ): ?>
+	      <?php $step = 1; ?>
+	      	<ul>
+	      	<?php while( have_rows('etapes') ): the_row();
 
-      		$name = get_sub_field('nom_de_letape');
-      		$content = get_sub_field('contenu_de_letape');
-      		$image = get_sub_field('image_de_letape');
+	      		// vars
 
-      		?>
+	      		$name = get_sub_field('nom_de_letape');
+	      		$content = get_sub_field('contenu_de_letape');
+	      		$image = get_sub_field('image_de_letape');
 
-      		<li>
-              <?php echo $step;
-                    $step += 1;
-              ?>
-      				<h4><?php echo $name; ?></h4>
-              <p><?php echo $content; ?></p>
-      				<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" />
-      		</li>
-      	<?php endwhile; ?>
-      	</ul>
+	      		?>
 
-      <?php endif; ?>
+	      		<li>
+	              <?php echo $step;
+	                    $step += 1;
+	              ?>
+	      				<h4><?php echo $name; ?></h4>
+	              <p><?php echo $content; ?></p>
+	      				<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" />
+	      		</li>
+	      	<?php endwhile; ?>
+	      	</ul>
+
+	      <?php endif; ?>
+
+			</section>
       <!-- /Steps of the documentation -->
 
       <!-- Dynamic content -->
-			<?php the_content(); ?>
+			<section>
+				<?php the_content(); ?>
+			</section>
       <!-- /Dynamic content -->
 
-
-
       <!-- comments of the post-->
-			<?php get_template_part('components/comments'); ?>
-
-		</article>
-		<!-- /article -->
-
-	<?php endwhile; ?>
-
-	<?php else: ?>
-
-		<!-- article -->
-		<article>
-
-			<h1><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h1>
-
-		</article>
-		<!-- /article -->
-
-	<?php endif; ?>
+			<section>
+				<?php get_template_part('components/comments'); ?>
+			</section>
 
 
-
-	</section>
       </div>
 	<!-- /section -->
 	</main>
