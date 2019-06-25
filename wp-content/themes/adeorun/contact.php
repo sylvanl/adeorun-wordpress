b5b0c7604aa7eb439f1e2631555e058fb1a52d8a
Alexandre Lafouasse <a_lafouasse@etu-webschoolfactory.gt>
2019-06-25 10:12:02 +0200

    Contact aka petite victoire

---
+++ wp-content/themes/adeorun/contact.php
@@ -0,0 +1,19 @@
+<?php /* Template Name: Contact */ ?>
+
+<?php get_header(); ?>
+
+<body>
+  <div class="container">
+
+    <div class="page_title">
+      <h1><?php the_title(); ?></h1>
+      <p><?php the_content(); ?></p>
+    </div>
+
+  </div>
+</body>
+
+
+
+
+<?php get_footer(); ?>
