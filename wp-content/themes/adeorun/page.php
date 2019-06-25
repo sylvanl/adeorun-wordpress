a8d8614345f77ec6f7fce8c1bc8d423cb50df0c6
Sylvan Lavallee <geckoweb.webmaster@gmail.com>
2019-06-25 09:56:50 +0200

    Update page

--- wp-content/themes/adeorun/page.php
+++ wp-content/themes/adeorun/page.php
@@ -1,37 +1,36 @@
 <?php get_header(); ?>
 
 	<main role="main">
-		<!-- section -->
-		<section>
+		
+		<div class="container">
 
-			<h1><?php the_title(); ?></h1>
+				<h1><?php the_title(); ?></h1>
 
-			<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+				<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
-				<!-- article -->
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<!-- article -->
+					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-					<?php the_content(); ?>
+						<?php the_content(); ?>
 
-				</article>
-				<!-- /article -->
+					</article>
+					<!-- /article -->
 
-			<?php endwhile; ?>
+				<?php endwhile; ?>
 
-			<?php else: ?>
+				<?php else: ?>
 
-				<!-- article -->
-				<article>
+					<!-- article -->
+					<article>
 
-					<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
+						<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
 
-				</article>
-				<!-- /article -->
+					</article>
+					<!-- /article -->
 
-			<?php endif; ?>
+				<?php endif; ?>
 
-		</section>
-		<!-- /section -->
+		</div>
 	</main>
 
 <?php get_footer(); ?>
