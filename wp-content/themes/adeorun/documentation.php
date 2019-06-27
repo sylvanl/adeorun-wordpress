e6f747228167185fe221d9fcf15b48c59918eefb
Sylvan Lavallee <geckoweb.webmaster@gmail.com>
2019-06-27 09:50:55 +0200

    Update documentation.php

--- wp-content/themes/adeorun/documentation.php
+++ wp-content/themes/adeorun/documentation.php
@@ -2,42 +2,35 @@
 
 <?php get_header(); ?>
 
-		<div id="container">
-			<div id="content" role="main">
+<?php $args = array(
+    'post_type' => 'post',
+    'post_status' => 'publish',
+    'category_name' => 'adeorun-news',
+    'posts_per_page' => 5,
+		);
+		$arr_posts = new WP_Query( $args );
+		
+		if ( $arr_posts->have_posts() ) :
+		
+				while ( $arr_posts->have_posts() ) :
+						$arr_posts->the_post();
+						?>
+						<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+								<?php
+								if ( has_post_thumbnail() ) :
+										the_post_thumbnail();
+								endif;
+								?>
+								<header class="entry-header">
+										<h1 class="entry-title"><?php the_title(); ?></h1>
+								</header>
+								<div class="entry-content">
+										<?php the_excerpt(); ?>
+										<a href="<?php the_permalink(); ?>">Read More</a>
+								</div>
+						</article>
+						<?php
+				endwhile;
+		endif;
 
-			<?php
-			// get all the categories from the database
-			$terms = get_terms( array(
-				'taxonomy' => 'documentation_tags',
-				'hide_empty' => false,
-			) );
-
-				// loop through the categries
-				foreach ($terms as $term) {
-
-					// setup the cateogory ID
-					$term_id= $term->term_id;
-					echo($term_id);
-					// Make a header for the cateogry
-					echo "<h2>".$term->name."</h2>";
-					// create a custom wordpress query
-					$loop = new WP_Query( array(
-					    'posts_per_page'  => 5,
-					    'category_name'   => $term->slug,
-					    'post__not_in'    => array( get_the_ID() )
-					  ) );
-					// start the wordpress loop!
-					if ($loop->have_posts()) : while ($loop->have_posts()) : $loop->the_post(); ?>
-
-						<?php // create our link now that the post is setup ?>
-						<a href="<?php the_permalink();?>"><?php the_title(); ?></a>
-						<?php echo '<hr/>'; ?>
-
-					<?php endwhile; endif; // done our wordpress loop. Will start again for each category ?>
-				<?php } // done the foreach statement ?>
-
-
-			</div><!-- #content -->
-		</div><!-- #container -->
-
-<?php get_footer(); ?>
+get_footer(); ?>
