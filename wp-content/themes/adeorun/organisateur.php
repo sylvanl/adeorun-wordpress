bf50b541cd0c2a211acb7de0b8ccf999ce4bf0ac
Alexandre Lafouasse <a_lafouasse@etu-webschoolfactory.gt>
2019-06-27 11:31:23 +0200

    Organisateur.php = done

--- wp-content/themes/adeorun/organisateur.php
+++ wp-content/themes/adeorun/organisateur.php
@@ -8,27 +8,45 @@
     <section>
       <h1><?php the_content(); ?></h1>
 
-      <?php if( have_rows('type_organisateur') ): ?>
-      <?php $type = 1; ?>
+      <div class="row">
+
+        <?php if( have_rows('type_organisateur') ): ?>
+        <?php $type = 1; ?>
+        <?php $row_count = count(get_field('type_organisateur')); ?>
 
         <?php while( have_rows('type_organisateur') ): the_row();
 
-      // Variables
+        // Variables
 
-          $image = get_sub_field('image_type_organisateur');
-          $title = get_sub_field('titre_type_organisateur');
-          $description = get_sub_field('description_type_organisateur');
-        ?>
+            $image = get_sub_field('image_type_organisateur');
+            $title = get_sub_field('titre_type_organisateur');
+            $description = get_sub_field('description_type_organisateur');
+          ?>
 
-      	  <div>
-      	    <?php $type += 1; ?>
-              <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" />
-      	      <h4><?php echo $title; ?></h4>
-      	      <p><?php echo $description; ?></p>
-          </div>
+            <?php if($row_count > 4) : ?>
 
-        <?php endwhile; ?>
-      <?php endif; ?>
+          	  <div class='col s3'>
+          	    <?php $type += 1; ?>
+                  <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" />
+          	      <h4><?php echo $title; ?></h4>
+          	      <p><?php echo $description; ?></p>
+              </div>
+
+            <?php else : ?>
+
+              <div>
+                <?php $type += 1; ?>
+                  <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" />
+                  <h4><?php echo $title; ?></h4>
+                  <p><?php echo $description; ?></p>
+              </div>
+
+            <?php endif; ?>
+
+
+          <?php endwhile; ?>
+        <?php endif; ?>
+      </div>
 
     </section>
 
