bc7d57e760ae134fc8148325b5aaccb7b2fd847d
Alexandre Lafouasse <a_lafouasse@etu-webschoolfactory.gt>
2019-06-27 16:07:37 +0200

    Change name of "Outils" and single-outil.php tweak

--- wp-content/themes/adeorun/single-outil.php
+++ wp-content/themes/adeorun/single-outil.php
@@ -1,16 +1,16 @@
 <?php get_header(); ?>
 
+<?php $post_type = get_post_type_object(get_post_type($post)); ?>
 	<main role="main">
     <div class="container">
 
 					<!-- post title -->
 					  <section>
 
-		    			<h1>
+							<h1><?php echo $post_type->labels->name; ?></h1>
+		    			<h2>
 		    				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-		    			</h1>
-
-		          <p><?php the_excerpt(); ?></p>
+		    			</h2>
 
 						</section>
 					<!-- /post title -->
