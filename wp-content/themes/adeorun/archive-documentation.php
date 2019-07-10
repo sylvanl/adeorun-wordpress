db61638ba934f1b1a0c61bd0a77b285e7feaaa6b
Alexandre Lafouasse <a_lafouasse@etu-webschoolfactory.gt>
2019-07-10 16:39:15 +0200

    Template de page de taxonomie - Documentation tags

--- wp-content/themes/adeorun/archive-documentation.php
+++ wp-content/themes/adeorun/archive-documentation.php
@@ -56,6 +56,29 @@
 		<?php }
 		 } } ?>
 		<?php wp_reset_query(); ?>
+
+<?php
+		$terms = get_terms( 'documentation_tags' );
+
+echo '<ul>';
+
+foreach ( $terms as $term ) {
+
+    // The $term is an object, so we don't need to specify the $taxonomy.
+    $term_link = get_term_link( $term );
+
+    // If there was an error, continue to the next term.
+    if ( is_wp_error( $term_link ) ) {
+        continue;
+    }
+
+    // We successfully got a link. Print it out.
+    echo '<li><a href="' . esc_url( $term_link ) . '">' . $term->name . '</a></li>';
+}
+
+
+echo '</ul>'; ?>
+
 	</div>
 </div>
 <?php get_footer(); ?>
