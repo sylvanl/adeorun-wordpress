7af406b97bbab93391d6937c147b23842fe14bf7
Alexandre Lafouasse <a_lafouasse@etu-webschoolfactory.gt>
2019-07-17 09:27:57 +0200

    new front-page.php

--- wp-content/themes/adeorun/front-page.php
+++ wp-content/themes/adeorun/front-page.php
@@ -7,68 +7,22 @@
   <div class="container">
 
     <!-- Thumbmail Page d'accueil -->
-    <section>
-
       <div class="main_block">
+
         <div class="main_block_search ">
-
-          <h2>Recherchez un évènement</h2>
-
-            <label for="event_type">Type d'évènement</label>
-            <textarea name="event_type" rows="8" cols="80"></textarea>
-
-            <label for="event_date">Dates</label>
-            <textarea name="event_date" rows="8" cols="80"></textarea>
-
-            <label for="event_location">Autour de</label>
-            <textarea name="event_type" rows="8" cols="80"></textarea>
-
-            <button type="submit" name="button">Rechercher</button>
-
+          <h2>Recherchez un événement</h2>
+          <textarea name="event_search" rows="8" cols="80" placeholder="Rechercher un événement..."></textarea>
+          <button class="btn waves-effect waves-light" type="submit" name="action">Rechercher</button>
+          <button class="btn waves-effect waves-light" type="submit" name="action">Me surprendre</button>
         </div>
 
-        <div class="main_block_event">
-          <a href="<?php echo $lien_evenement_page_accueil; ?>"><p><?php echo $evenement_page_acceuil; ?></p></a>
+        <div class="main_block_side">
+          <h3><?php echo $home_title; ?></h3>
+          <p><?php echo $home_description; ?></p>
+          <a class="waves-effect waves-light btn" href="<?php echo $home_button_link; ?>"><?php echo $home_button_text; ?></a>
         </div>
+
       </div>
-
-    </section>
-
-    <section>
-      <h2><?php echo $premier_titre; ?> </h2>
-    </section>
-
-    <section>
-      <h2><?php echo $deuxieme_titre; ?> </h2>
-
-      	      <?php if( have_rows('type_organisateur') ): ?>
-      	      <?php $type = 1; ?>
-
-      	      	<?php while( have_rows('type_organisateur') ): the_row();
-
-      	      		// vars
-
-      	      		$image = get_sub_field('image_type_organisateur');
-      	      		$title = get_sub_field('titre_type_organisateur');
-      	      		$description = get_sub_field('description_type_organisateur');
-
-      	      		?>
-
-      	      		<div>
-
-      	              <?php
-      	                    $type += 1;
-      	              ?>
-                      <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" />
-      	      				<h4><?php echo $title; ?></h4>
-      	              <p><?php echo $description; ?></p>
-
-                  </div>
-                  
-      	      	<?php endwhile; ?>
-      	      <?php endif; ?>
-
-    </section>
 
   </div>
 </main>
