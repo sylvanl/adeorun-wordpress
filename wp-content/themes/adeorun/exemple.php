161b0423361a910f0306cb4d25db063c2b2d0498
Alexandre Lafouasse <a_lafouasse@etu-webschoolfactory.gt>
2019-07-29 11:14:12 +0200

    CSS - Example.php

--- wp-content/themes/adeorun/exemple.php
+++ wp-content/themes/adeorun/exemple.php
@@ -5,11 +5,6 @@
 <?php
   // vars
   $temoignage = get_field('temoignage');
-  if( have_rows('temoignage') ):
-    while( have_rows('temoignage') ) : the_row(); 
-      echo the_sub_field('titre');
-    endwhile;
-  endif;
   $bouton = get_field('bouton_formulaire');
   $fonctionnalites = get_field('fonctionnalites_adeorun');
   $evenement_titre = get_field('evenements_titre');
@@ -19,55 +14,47 @@
 <main role="main">
   <div class="container">
 
-    <div class="page_title">
+    <div class="page_title section">
       <h1><?php the_title(); ?></h1>
-      <p><?php echo $evenement_titre; ?></p>
+      <p class="center-align"><?php the_excerpt(); ?></p>
     </div>
 
-    <div class="page_thumbnail">
-      <?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
-        <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-          <?php the_post_thumbnail(); // Fullsize image for the single post ?>
-        </a>
-      <?php endif; ?>
-    </div>
+    <div class="page_fonctionnalites section">
 
-    <div class="page_temoignage">
-
-      <div class="page_temoignage_titre">
-        <h3><?php echo $temoignage['titre']; ?></h3>
-      </div>
-
-      <div class="page_temoignage_contenu">
-        <img src="<?php echo $teimoignage['image']['url']; ?>" alt="">
-        <p><?php echo $temoignage['description'] ?></p>
-        <p><?php echo $temoignage['contenu'] ?></p>
-      </div>
-
-    </div>
-
-    <div class="page_button_formulaire">
-      <button class="waves-effect waves-light btn" url="<?php echo $bouton['url']; ?>"><?php echo $bouton['texte']; ?></a>
-    </div>
-
-    <div class="page_fonctionnalités">
-
-      <h3><?php echo $fonctionnalites['titre']; ?></h3>
-
+      <?php $advantage = 1; ?>
       <?php if( have_rows('fonctionnalites_adeorun') ): while ( have_rows('fonctionnalites_adeorun') ) : the_row(); ?>
-
         <?php if( have_rows('fonctionnalites') ): while ( have_rows('fonctionnalites') ) : the_row();
-
-          // vars
           $name = get_sub_field('titre');
           $image = get_sub_field('image');
+          $description = get_sub_field('description');
+         ?>
 
-          ?>
-
-          <div>
+  				<?php if($advantage % 2 != 0) :  ?>
+          <div class="row">
+            <div class="col s7">
               <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" />
-              <p><?php echo $name; ?></p>
+            </div>
+
+              <div class="col s5">
+                <h3><?php echo $name; ?></h3>
+                <p><?php echo $description; ?></p>
+                <button class="waves-effect waves-light btn" url="<?php echo $bouton['url']; ?>"><?php echo $bouton['texte']; ?></a>
+              </div>
           </div>
+          <?php else : ?>
+            <div class="row">
+              <div class="col s5">
+                <h3><?php echo $name; ?></h3>
+                <p><?php echo $description; ?></p>
+                <button class="waves-effect waves-light btn" url="<?php echo $bouton['url']; ?>"><?php echo $bouton['texte']; ?></a>
+              </div>
+
+              <div class="col s7">
+                <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" />
+              </div>
+          </div>
+          <?php endif; ?>
+          <?php $advantage += 1; ?>
 
           <?php endwhile; ?>
           <?php endif; ?>
@@ -76,28 +63,25 @@
 
     </div>
 
-    <div class="page_evenements">
-      <h3><?php echo $evenement_titre; ?></h3>
-    </div>
+    <div class="divider"></div>
+      <div class="page_contenu section">
+        <p><?php the_content(); ?></p>
+      </div>
 
-    <div class="page_contenu">
-      <p><?php the_content(); ?></p>
-    </div>
-
-    <div class="page_avantages">
+    <div class="divider"></div>
+      <div class="page_avantages section">
       <h3><?php echo $avantages['titre']; ?></h3>
 
+      <div class="row">
       <?php if( have_rows('avantages_adeorun') ): while ( have_rows('avantages_adeorun') ) : the_row(); ?>
-
         <?php if( have_rows('avantages') ): while ( have_rows('avantages') ) : the_row();
 
           // vars
           $name = get_sub_field('titre');
           $image = get_sub_field('image');
-
           ?>
 
-          <div>
+          <div class="small_clickable col s3">
               <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" />
               <p><?php echo $name; ?></p>
           </div>
@@ -107,15 +91,51 @@
         <?php endwhile; ?>
         <?php endif; ?>
 
+        </div>
     </div>
-
-    <div class="page_couts">
+    
+    <div class="page_couts section row">
       <h3><?php the_field('titre', 'option'); ?></h3>
       <p><?php the_field('contenu', 'option'); ?></p>
-      <p><?php the_field('cout_fixe', 'option'); ?></p>
-      <p><?php the_field('cout_variable', 'option'); ?></p>
+
+      <div class="page_couts_aside col s4 offset-s9">
+        <p>Frais de services à la charge du participant*</p>
+        <p><?php the_field('cout_fixe', 'option'); ?> €</p>
+        <p>*<?php the_field('cout_variable', 'option'); ?>% + <?php the_field('cout_fixe', 'option'); ?>€ (TTC par participant) </p>
+      </div>
+    </div>
+
+
+    <div class="page_temoignage section row">
+
+      <div class="col s7 card medium section">
+        <div class="page_temoignage_contenu">
+          <p><?php echo $temoignage['contenu']; ?></p>
+        </div>
+
+        <div class="page_temoignage_profil section">
+          <div class="col s3">
+            <img src="<?php echo $teimoignage['profile_picture']['url']; ?>" alt="">
+          </div>
+
+          <div class="col s9">
+            <p><?php echo $temoignage['name']; ?></p>
+            <p><?php echo $temoignage['description']; ?></p>
+          </div>
+        </div>
+
+        <div class="page_button_formulaire section">
+          <button class="waves-effect waves-light btn" url="<?php echo $bouton['url']; ?>"><?php echo $bouton['texte']; ?></a>
+        </div>
+
+      </div>
 
     </div>
+
+    <div class="page_evenements section">
+      <h3><?php echo $evenement_titre; ?></h3>
+    </div>
+
 
   </div>
 
