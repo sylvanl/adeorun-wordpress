07be3871209a17050c60bf5fb30341a0da558f8d
Alexandre Lafouasse <a_lafouasse@etu-webschoolfactory.gt>
2019-07-29 11:52:59 +0200

    CSS - single-post.php

--- wp-content/themes/adeorun/single-post.php
+++ wp-content/themes/adeorun/single-post.php
@@ -2,67 +2,62 @@
 
 	<main role="main">
 
-
 						<!-- sidebar-->
-							<?php get_template_part('components/sidebar'); ?>
+							<div class="sidebar_article">
+								<?php get_template_part('components/sidebar'); ?>
+							</div>	
 						<!-- /sidebar-->
-
+						
 		<div class="container">
 
 						<!-- post title -->
-						<section>
+						<div class="section">
 							<h1 class="left-align"><?php the_title(); ?></h1>
 							<p><?php the_excerpt(); ?></p>
-						</section>
+						</div>
 						<!-- /post title -->
 
 						<!-- post & author details -->
-						<section>
-							<div class="post_details">
+							<div class="post_details section">
 								<?php get_template_part('components/author-single'); ?>
 							</div>
-						</section>
 						<!-- /post & author details -->
 
 						<!-- post thumbnail -->
-						<section>
-
+						<div class="section">
 							<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
 								<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
 									<?php the_post_thumbnail(); // Fullsize image for the single post ?>
 								</a>
 							<?php endif; ?>
-
-						</section>
+						</div>
 						<!-- /post thumbnail -->
 
 
-						<section>
+						<div class="article_content section">
 						 	<?php the_content(); // Dynamic Content ?>
-					 	</section>
+					 	</div>
 
-						<!-- tags & categories-->
-						<section>
-							<?php the_tags( __( 'Tags: ', 'html5blank' ), ', ', '<br>'); // Separated by commas with a line break at the end ?>
-							<p><?php _e( 'Categories: ', 'html5blank' ); the_category(', '); // Separated by commas ?></p>
-						</section>
 
 
 						<!-- author of the post-->
-						<section>
+						<div class="section">
 							<?php get_template_part('components/author-bio'); ?>
-						</section>
+						</div>
 
 						<div class="divider"></div>
 
 						<!-- comments of the post-->
-						<section>
+						<div class="section">
 							<?php get_template_part('components/comments'); ?>
-						</section>
+						</div>
+						
+
+						<div class="divider"></div>
 
 
 			<!-- Show 3 lastest posts -->
-						<section>
+						<div class="section">
 
 							<h3>A découvrir également</h3>
 
@@ -82,15 +77,9 @@
 
 										</div>
 										<div class="card-content">
-											<span class="card-title"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); // Fullsize image for the single post ?> </a></span>
-											<p class="author_pic"><?php echo get_avatar( get_the_author_meta('<? php the_author_ID ?>') , 32 ); ?> </p>
+											<p><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?> </a></p>
 											<p class="author_name"><?php the_author_posts_link(); ?></p>
-											<p class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></p>
-										</div>
-										<div class="card-action">
-											<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-												Voir l'article
-											</a>
+											<p class="date"><?php the_time('F j, Y'); ?></p>
 										</div>
 									</div>
 								</div>
