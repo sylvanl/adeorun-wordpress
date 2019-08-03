ad4239c8e5375896a363d659a76ed95d110706c7
Alexandre Lafouasse <a_lafouasse@etu-webschoolfactory.gt>
2019-08-03 19:13:33 +0200

    Update sport.php

--- wp-content/themes/adeorun/sport.php
+++ wp-content/themes/adeorun/sport.php
@@ -15,15 +15,13 @@
         <?php while( have_rows('sport') ): the_row();
 
         // Variables
-
             $image = get_sub_field('sport_icon');
             $title = get_sub_field('sport_title');
           ?>
 
           	  <div class='col s3 large-clickable hoverable'>
                   <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" />
-          	      <p class="orga-title"><?php echo $title; ?></h3>
-          	      <p class="subtitle grey-text"><?php echo $description; ?><i class="far fa-arrow-right right black-text"></i></p>
+          	      <p class="orga-title"><?php echo $title; ?><i class="far fa-arrow-right right black-text"></i></p>
               </div>
 
           <?php endwhile; ?>
