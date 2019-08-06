0c14828751243c5d843fb73d2c847946113c9c3b
Alexandre Lafouasse <a_lafouasse@etu-webschoolfactory.gt>
2019-08-06 16:26:33 +0200

    Update exemple.php

--- wp-content/themes/adeorun/exemple.php
+++ wp-content/themes/adeorun/exemple.php
@@ -154,7 +154,6 @@
           <div class="col s3">
             <a class="black-text no-underline" href="<?php echo the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
               <?php the_post_thumbnail(); ?>
-              <?php the_terms(); ?>
               <p class="bold"><?php the_title(); ?></p>
               <p><?php the_field('start_date'); ?></p>
               <p><?php the_field('city');?> , <?php the_field('region_name'); ?></p>
