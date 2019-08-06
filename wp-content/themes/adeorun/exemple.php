94dc5aa5b89e0906ccdf15be3a7f9984aa52aef2
Alexandre Lafouasse <a_lafouasse@etu-webschoolfactory.gt>
2019-08-06 15:53:21 +0200

    Update exemple.php

--- wp-content/themes/adeorun/exemple.php
+++ wp-content/themes/adeorun/exemple.php
@@ -142,7 +142,7 @@
           'post_status' => 'publish',
           'tax_query' => array(
               array(
-                'taxonomy' => $sport_type,
+                'taxonomy' => $sport_type["slug"],
               ),
             ),
 
