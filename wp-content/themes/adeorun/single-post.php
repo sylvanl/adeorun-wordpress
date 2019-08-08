0e546afe03880e242a285c41ba998a3d94d8de5b
Alexandre Lafouasse <a_lafouasse@etu-webschoolfactory.gt>
2019-08-08 10:11:52 +0200

    Update single-post.php

--- wp-content/themes/adeorun/single-post.php
+++ wp-content/themes/adeorun/single-post.php
@@ -25,10 +25,8 @@
 						<!-- post thumbnail -->
 						<div class=" row section">
 							<div class="col s12 center-align thumbnail">
-							<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
-								<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-									<?php the_post_thumbnail(); // Fullsize image for the single post ?>
-								</a>
+							<?php if ( has_post_thumbnail()) : ?>
+								<?php the_post_thumbnail(); ?>
 							<?php endif; ?>
 							</div>
 						</div>
