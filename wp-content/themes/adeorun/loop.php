c3f3039602ea5d7fcc36c711bab07b9a9434e7c2
Alexandre Lafouasse <a_lafouasse@etu-webschoolfactory.gt>
2019-08-09 13:59:26 +0200

    Template search

--- wp-content/themes/adeorun/loop.php
+++ wp-content/themes/adeorun/loop.php
@@ -1,34 +1,35 @@
 <?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
-	<!-- article -->
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-		<!-- post thumbnail -->
-		<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
-			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-				<?php the_post_thumbnail(array(120,120)); // Declare pixel size you need inside the array ?>
-			</a>
-		<?php endif; ?>
-		<!-- /post thumbnail -->
+          <div class="col s3">
+            <?php the_favorites_button($post_id, $site_id); ?>
+            <a class="black-text no-underline" href="<?php echo the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
 
-		<!-- post title -->
-		<h2>
-			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-		</h2>
-		<!-- /post title -->
+              <?php 
+                foreach (get_the_terms(get_the_ID(), 'evenement_tags') as $cat) {
+                    echo '<span class="new badge grey lighten-1" data-badge-caption="">' .  $cat->name .'</span>';
+                } 
+              ?>
 
-		<!-- post details -->
-		<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
-		<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-		<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
-		<!-- /post details -->
+              <?php the_post_thumbnail(); ?>
+              <p class="bold"><?php the_title(); ?></p>
+              <p><?php the_field('start_date'); ?></p>
+              <p><?php the_field('city');?> , <?php the_field('region_name'); ?></p>
 
-		<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
+              <?php if( have_rows('circuit') ): ?>
+                <div>
+                <?php while( have_rows('circuit') ): the_row();
+                  $distance = get_sub_field('distance'); ?>
+                <div>
+                  <p><?php echo $distance; ?></p>
+                  <p>Km</p>
+                </div>
+                <?php endwhile; ?> 
 
-		<?php edit_post_link(); ?>
-
-	</article>
-	<!-- /article -->
+                </div>
+               <?php endif; ?> 
+            </a>
+          </div>
 
 <?php endwhile; ?>
 
